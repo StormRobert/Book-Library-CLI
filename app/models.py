4dc6870f5934bc53e6db6fa6196154fe3290139a
fay <wahomeimani@gmail.com>
2023-12-13 16:01:58 +0300

    user table

--- app/models.py
+++ app/models.py
@@ -1,6 +1,23 @@
-from sqlalchemy import create_engine
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
+from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
-
-engine = create_engine('sqlite:///library.db')
+from sqlalchemy.sql import func
 
 Base = declarative_base()
+
+
+
+
+
+
+
+
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    username = Column(String, nullable=False, unique=True)
+    created_at = Column(DateTime, server_default=func.now())
+    updated_at = Column(DateTime, onupdate=func.now())
+
+    # Define the relationship with Book
+    books = relationship('Book', back_populates='owner')
