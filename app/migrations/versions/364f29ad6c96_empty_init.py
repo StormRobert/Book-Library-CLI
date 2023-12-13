888cbe46d1ecbff1f9cc2bfc805ea6b067c98d20
mgkEdits <elijahnyasiando.mn@gmail.com>
2023-12-13 10:12:01 +0300

    migration initialization

---
+++ app/migrations/versions/364f29ad6c96_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: 364f29ad6c96
+Revises: 
+Create Date: 2023-12-13 10:06:57.733910
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '364f29ad6c96'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
