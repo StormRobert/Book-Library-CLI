95105c3d8a3a2ae0c9b01f2669f42799460760fe
fay <wahomeimani@gmail.com>
2023-12-15 20:58:21 +0300

    use of showall & pretty table

---
+++ app/models.py
@@ -0,0 +1,127 @@
+from sqlalchemy import create_engine, Column, Integer, String, Date, Boolean
+from sqlalchemy.orm import declarative_base, sessionmaker
+from datetime import datetime
+from prettytable import PrettyTable
+
+engine = create_engine('sqlite:///library.db', echo=False)
+
+Base = declarative_base()
+
+class Book(Base):
+    __tablename__ = 'books'
+
+    id = Column(Integer(), primary_key=True)
+    title = Column(String)
+    author = Column(String())
+    publication_date = Column(Date())
+    genre = Column(String())
+    availability = Column(Boolean())
+
+    def __init__(self, title, author, publication_date, genre, availability=True):
+        self.title = title
+        self.author = author
+        self.publication_date = datetime.strptime(publication_date, "%Y-%m-%d").date()
+        self.genre = genre
+        self.availability = availability
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer(), primary_key=True)
+    name = Column(String())
+    email = Column(String())
+    phone_number = Column(Integer())
+
+    def __init__(self, name, email, phone_number):
+        self.name = name
+        self.email = email
+        self.phone_number = phone_number
+
+class Book_checkout(Base):
+    __tablename__='bookCheckout'
+
+    id = Column(Integer(), primary_key=True)
+    bookID = Column(Integer())
+    userID = Column(Integer())
+    genre = Column(String())
+    checkoutDate = Column(Date())
+    returnDate = Column(Date())
+
+    def __init__(self, bookID, userID, genre, checkoutDate, returnDate):
+        self.bookID = bookID
+        self.userID = userID
+        self.genre = genre
+        self.checkoutDate = checkoutDate
+        self.returnDate = returnDate
+
+
+Base.metadata.create_all(engine)
+
+def showall(data, table_name):
+    """Display all elements in the given data."""
+    if not data:
+        print(f"No records found in {table_name}")
+        return
+
+    table = PrettyTable()
+    table.field_names = data[0].__dict__.keys()
+
+    for item in data:
+        table.add_row(item.__dict__.values())
+
+    print(f"{table_name.capitalize()}:\n{table}")
+# ... (previous code)
+
+# Create a session
+Session = sessionmaker(bind=engine)
+session = Session()
+
+# Insert data into the 'books' table for testing
+great_Gatsby = Book(
+    title="The Great Gatsby",
+    author="F. Scott Fitzgerald",
+    publication_date="1925-04-10",
+    genre="Fiction",
+    availability=True
+)
+
+session.add(great_Gatsby)
+session.commit()
+
+# Query and display the contents of the 'books' table
+books = session.query(Book).all()
+showall(books, 'books')
+
+# Insert data into the 'users' table for testing
+new_user = User(
+    name="John Doe",
+    email="john.doe@example.com",
+    phone_number=1234567890
+)
+
+session.add(new_user)
+session.commit()
+
+# Query and display the contents of the 'users' table
+users = session.query(User).all()
+showall(users, 'users')
+
+# Insert data into the 'bookCheckout' table for testing
+checkout_entry = Book_checkout(
+    bookID=1,  # Assuming bookID corresponds to the ID of 'The Great Gatsby'
+    userID=1,  # Assuming userID corresponds to the ID of 'John Doe'
+    genre="Fiction",
+    checkoutDate=datetime.now(),
+    returnDate=datetime.now()
+)
+
+session.add(checkout_entry)
+session.commit()
+
+# Query and display the contents of the 'bookCheckout' table
+book_checkouts = session.query(Book_checkout).all()
+showall(book_checkouts, 'bookCheckouts')
+
+# Close the session
+session.close()
+
