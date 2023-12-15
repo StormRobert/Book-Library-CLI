66bbf1800502b89a2775e2aef9a26e9997c80635
mgkEdits <elijahnyasiando.mn@gmail.com>
2023-12-15 06:57:36 +0300

    Add book-checkout class

--- app/models.py
+++ app/models.py
@@ -48,6 +48,31 @@ class User(Base):
                 + f"{self.email}"\
                 + f"{self.phone_number}"
       
+
+class Book_checkout(Base):
+    __tablename__='bookCheckout'
+
+    id = Column(Integer(), primary_key=True)
+    bookID = Column(Integer())
+    userID = Column(Integer())
+    genre = Column(String())
+    checkoutDate = Column(Date())
+    returnDate = Column(Date())
+
+    def __init__(self, id, bookID, userID, genre, checkoutDate, returnDate):
+        self.bookID = bookID
+        self.userID = userID
+        self.genre = genre
+        self.checkoutDate = checkoutDate
+        self.returnDate = returnDate
+    
+    def __repr__(self):
+           return f"{self.bookID}"\
+                + f"{self.userID}"\
+                + f"{self.genre}"\
+                + f"{self.checkoutDate}"\
+                + f"{self.returnDate}"
+      
       
 Base.metadata.create_all(engine)
 
