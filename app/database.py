f8c014f4605f22078446ec66a807579558373579
mgkEdits <elijahnyasiando.mn@gmail.com>
2023-12-17 23:13:59 +0300

    seed data

--- app/database.py
+++ app/database.py
@@ -1,71 +1,106 @@
-from sqlalchemy import create_engine, Column, Integer, String, Date, Boolean, ForeignKey
-from sqlalchemy.orm import declarative_base, sessionmaker, relationship
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from models import Book, User, Book_checkout
 from datetime import datetime
 
+database_url='sqlite:///library.db'
+engine = create_engine(database_url, echo=False)
+Session = sessionmaker(bind=engine)
+session = Session()
 
-engine = create_engine('sqlite:///library.db', echo=False)
-Base = declarative_base()
+# -----------------------------Methods for the book table
+def get_all_books(session):
+    return session.query(Book).all()
 
-class Book(Base):
-    __tablename__ = 'books'
+def add_book(session, title, author, publication_date, genre, availability=True):
+    new_book = Book(title=title, author=author, publication_date=publication_date, genre=genre, availability=availability)
+    session.add(new_book)
+    session.commit()
 
-    id = Column(Integer(), primary_key=True)
-    title = Column(String)
-    author = Column(String())
-    publication_date = Column(Date())
-    genre = Column(String())
-    availability = Column(Boolean(), default=True)
+def update_book(session, book_id, title=None, author=None, publication_date=None, genre=None, availability=None):
+    book = session.query(Book).get(book_id)
+    if book:
+        if title:
+            book.title = title
+        if author:
+            book.author = author
+        if publication_date:
+            book.publication_date = publication_date
+        if genre:
+            book.genre = genre
+        if availability is not None:
+            book.availability = availability
+        session.commit()
+        return (f"Book with ID {book_id}updated.")
+    else:
+        print(f"Book with ID {book_id} not found.")
 
-    checkouts = relationship('Book_checkout', back_populates='book')
+def delete_book(session, book_id):
+    book = session.query(Book).get(book_id)
+    if book:
+        session.delete(book)
+        session.commit()
+        return f"{book.title} is being deleted from the database, check the table again for confirmation"
+    else:
+        print(f"Book with ID {book_id} not found.")
 
-    def __init__(self, title, author, publication_date, genre, availability=None):
-        self.title = title
-        self.author = author
-        self.publication_date = publication_date
-        self.genre = genre
-        self.availability = availability
+# ------------------------Methods for the user table
+def get_all_users(session):
+    return session.query(User).all()
 
-    def __repr__(self):
-        return repr({"id": self.id, "title": self.title, "author": self.author, "publication_date": self.publication_date, "genre": self.genre, "availability": self.availability})
+def add_user(session, name, email, phone_number):
+    new_user = User(name=name, email=email, phone_number=phone_number)
+    session.add(new_user)
+    session.commit()
 
-class User(Base):
-    __tablename__ = 'users'
+def delete_user(session, user_id):
+    user = session.query(User).get(user_id)
+    if user:
+        session.delete(user)
+        session.commit()
+    else:
+        print(f"User with ID {user_id} not found.")
 
-    id = Column(Integer(), primary_key=True)
-    name = Column(String())
-    email = Column(String())
-    phone_number = Column(Integer())
+#------------------ Methods for the Book_checkout table
+def get_all_checkouts(session):
+    return session.query(Book_checkout).all()
 
-    checkouts = relationship('Book_checkout', back_populates='user')
+def checkout_book(session, book_id, user_id, genre, checkout_date=datetime.now()):
+    book = session.query(Book).get(book_id)
+    user = session.query(User).get(user_id)
+    if book and user:
+        new_checkout = Book_checkout(book=book, user=user, genre=genre, checkoutDate=checkout_date)
+        session.add(new_checkout)
+        session.commit()
+    else:
+        print(f"Book with ID {book_id} or User with ID {user_id} not found.")
 
-    def __init__(self, name, email, phone_number):
-        self.name = name
-        self.email = email
-        self.phone_number = phone_number
-        
+def return_book(session, checkout_id, return_date=datetime.now()):
+    checkout = session.query(Book_checkout).get(checkout_id)
+    if checkout:
+        checkout.returnDate = return_date
+        session.commit()
+    else:
+        print(f"Checkout with ID {checkout_id} not found.")
 
-    def __repr__(self):
-        return repr({"id": self.id, "name": self.name, "email": self.email, "phone_number": self.phone_number})
+# Other methods
+def recommend_books(session, user_id):
+    user_checkouts = session.query(Book_checkout).filter_by(userID=user_id).all()
 
-class Book_checkout(Base):
-    __tablename__='bookCheckouts'
+    if not user_checkouts:
+        print("No past checkouts found for this user.")
+        return
 
-    id = Column(Integer(), primary_key=True)
-    bookID = Column(Integer(), ForeignKey('books.id'))
-    userID = Column(Integer(), ForeignKey('users.id') )
-    genre = Column(String())
-    checkoutDate = Column(Date())
-    returnDate = Column(Date())
+    # Count the occurrences of each genre in the user's checkouts
+    genre_counts = {}
+    for checkout in user_checkouts:
+        genre = checkout.genre
+        genre_counts[genre] = genre_counts.get(genre, 0) + 1
 
-    book = relationship('Book', back_populates='checkouts')
-    user = relationship('User', back_populates='checkouts') 
+    # Find the most frequently checked-out genre
+    most_frequent_genre = max(genre_counts, key=genre_counts.get)
+    print(f"Recommended books for user {user_id} based on past checkouts in the genre '{most_frequent_genre}':")
+    
+    # Recommend books of the most frequent genre
+    return session.query(Book).filter_by(genre=most_frequent_genre).all()
 
-    def __init__(self,book,user, genre, checkoutDate, returnDate):
-        self.book = book
-        self.user = user
-        self.genre = genre
-        self.checkoutDate = checkoutDate
-        self.returnDate = returnDate
-
-    def __repr__(self):
-        return repr({"id": self.id,"Book":self.book,  "genre": self.genre, "checkoutDate": self.checkoutDate, "returnDate": self.returnDate})
