2a2ba0ca16825ce793dac1f4493bc63cc589d4aa
Matthew <+>
2023-12-17 14:15:50 +0300

    dded books and user methods in database.py and initialized them in bookLibrary.py

---
+++ app/bookLibrary.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from datetime import datetime
+from models import Base, Book,User,Book_checkout
+from database import add_book,update_book, delete_book,add_user, delete_user
+
+if __name__ == "__main__":
+    engine = create_engine('sqlite:///library.db')
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    
+
+    #BOOK TABLE METHODS
+                # ADDING A BOOK TO THE TABLE
+    Vampire_diaries = Book(title="Vampire Diaries", author="MAtthew ",publication_date=datetime.strptime("1997-10-22", "%Y-%m-%d").date(), genre="Fiction", availability=1)
+    print(f" {add_book(session, Vampire_diaries)} ")
+                #UPDATING A BOOK THAT IS ALREADY IN THE DATABASE
+    existing_book = session.query(Book).filter_by(title="Vampire Diaries").first()
+    if existing_book:
+        existing_book.title = "Vampires"
+        existing_book.author = "ELIJAH"
+        existing_book.publication_date = datetime.strptime("1997-10-22", "%Y-%m-%d").date()
+        existing_book.genre = "Adventure"
+        existing_book.availability = 1
+        session.commit()
+        print(f"{update_book(session, existing_book.id, 'Vampires', 'ELIJAH', datetime.strptime('1997-10-22', '%Y-%m-%d').date(), 'Adventure', 1)}")
+    else:
+        print("Book not found")
+
+                #DELETING AN EXISTING BOOK
+    book = session.query(Book).filter_by(title="Streamlined holistic function").first()
+    if book:
+        print(f"{delete_book(session, book)}")
+    else:
+        print("Book non-existent because it's deleted")
+
+
+                #ADDING A NEW USER TO THE USERS TABLE
+    Messi = User(name="Messi", email="messi@gmail.com", phone_number=254722806547)
+    print(f"{add_user(session,Messi)}")
+                #DELETING AN EXISTING USER FROM THE USERS TABLE
+    Jacob = session.query(User).filter_by(name = "Jacob Little").first()
+    print({delete_user(session, Jacob)})
+
--- app/database.py
+++ app/database.py
@@ -1,31 +1,55 @@
-from models import Book,datetime
+from models import Book, User, Book_checkout , sessionmaker, create_engine, Base
 
-def create_book_table(base, engine):
-    base.metadata.create_all(engine)
+engine = create_engine('sqlite:///library.db')
+Session = sessionmaker(bind=engine)
+session = Session()
 
+Base.metadata.create_all(engine)
 def add_book(session, book):
-    session.add(book)
-    session.commit()
-    print(f"{book.title} by {book.author} added to db.")
+    existing_book = session.query(Book).filter_by(title=book.title).first()
+    if existing_book:
+        return "Book already exists"
+    else:
 
-def delete_book(session, book_id):
-    book = session.query(Book).filter_by(id=book_id).first()
-    if book:
+      session.add(book)
+    session.commit()
+    return f"Book {book.title} added successfully."
+    
+
+def delete_book(session, book):
         session.delete(book)
         session.commit()
-        print(f"Book with id {book_id} deleted from db.")
-    else:
-        print(f"Book not found.")
+        return f"{book.title} is being deleted from the database, check the table again for confirmation"
+        
 
 def update_book(session, book_id, new_title, new_author, new_publication_date, new_genre, new_availability):
     book = session.query(Book).filter_by(id=book_id).first()
     if book:
         book.title = new_title
         book.author = new_author
-        book.publication_date = datetime.strptime(new_publication_date, "%Y-%m-%d").date()
+        book.publication_date = new_publication_date
         book.genre = new_genre
         book.availability = new_availability
         session.commit()
-        print(f"Book with id {book_id} updated.")
+        return (f"Book with id {book_id}updated.")
+    
+def add_user(session, user):
+    existing_user = session.query(User).filter_by(name = user.name).first()
+    if existing_user:
+        return f"{user.name} exists"
     else:
-        print(f"Book not found")
+       session.add(user)
+       session.commit()  
+       return f"{user.name} added to the users table"  
+    
+def delete_user(session,user):
+    if user:
+        existing_user = session.query(User).filter_by(name = user.name).first()
+        if existing_user:
+            session.delete(user)
+            session.commit()
+            return f"{user.name} is being deleted from the database , check table to confirm"
+        else:
+            return f"{user.name} has been deleted from the database"
+    else:
+        return f"delete successfull"
