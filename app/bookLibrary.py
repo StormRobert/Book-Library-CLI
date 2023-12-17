4e196a0720c41c4443a611f2f4a33820b249c49b
mgkEdits <elijahnyasiando.mn@gmail.com>
2023-12-17 20:15:19 +0300

    library methods

--- app/bookLibrary.py
+++ app/bookLibrary.py
@@ -3,7 +3,16 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from datetime import datetime
 from models import Base, Book,User,Book_checkout
-from database import add_book,update_book, delete_book,add_user, delete_user
+from database import add_book, update_book, delete_book, get_all_Books, add_user, delete_user
+
+import typer
+from rich.console import Console
+from rich.table import Table
+
+console = Console()
+
+app = typer.Typer()
+
 
 if __name__ == "__main__":
     engine = create_engine('sqlite:///library.db')
@@ -11,37 +20,39 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    
+@app.command()
+def showBooks():
+    books = get_all_books()
 
-    #BOOK TABLE METHODS
-                # ADDING A BOOK TO THE TABLE
-    Vampire_diaries = Book(title="Vampire Diaries", author="MAtthew ",publication_date=datetime.strptime("1997-10-22", "%Y-%m-%d").date(), genre="Fiction", availability=1)
-    print(f" {add_book(session, Vampire_diaries)} ")
-                #UPDATING A BOOK THAT IS ALREADY IN THE DATABASE
-    existing_book = session.query(Book).filter_by(title="Vampire Diaries").first()
-    if existing_book:
-        existing_book.title = "Vampires"
-        existing_book.author = "ELIJAH"
-        existing_book.publication_date = datetime.strptime("1997-10-22", "%Y-%m-%d").date()
-        existing_book.genre = "Adventure"
-        existing_book.availability = 1
-        session.commit()
-        print(f"{update_book(session, existing_book.id, 'Vampires', 'ELIJAH', datetime.strptime('1997-10-22', '%Y-%m-%d').date(), 'Adventure', 1)}")
-    else:
-        print("Book not found")
+    # #BOOK TABLE METHODS
+    #             # ADDING A BOOK TO THE TABLE
+    # Vampire_diaries = Book(title="Vampire Diaries", author="MAtthew ",publication_date=datetime.strptime("1997-10-22", "%Y-%m-%d").date(), genre="Fiction", availability=1)
+    # print(f" {add_book(session, Vampire_diaries)} ")
+    #             #UPDATING A BOOK THAT IS ALREADY IN THE DATABASE
+    # existing_book = session.query(Book).filter_by(title="Vampire Diaries").first()
+    # if existing_book:
+    #     existing_book.title = "Vampires"
+    #     existing_book.author = "ELIJAH"
+    #     existing_book.publication_date = datetime.strptime("1997-10-22", "%Y-%m-%d").date()
+    #     existing_book.genre = "Adventure"
+    #     existing_book.availability = 1
+    #     session.commit()
+    #     print(f"{update_book(session, existing_book.id, 'Vampires', 'ELIJAH', datetime.strptime('1997-10-22', '%Y-%m-%d').date(), 'Adventure', 1)}")
+    # else:
+    #     print("Book not found")
 
-                #DELETING AN EXISTING BOOK
-    book = session.query(Book).filter_by(title="Streamlined holistic function").first()
-    if book:
-        print(f"{delete_book(session, book)}")
-    else:
-        print("Book non-existent because it's deleted")
+    #             #DELETING AN EXISTING BOOK
+    # book = session.query(Book).filter_by(title="Streamlined holistic function").first()
+    # if book:
+    #     print(f"{delete_book(session, book)}")
+    # else:
+    #     print("Book non-existent because it's deleted")
 
 
-                #ADDING A NEW USER TO THE USERS TABLE
-    Messi = User(name="Messi", email="messi@gmail.com", phone_number=254722806547)
-    print(f"{add_user(session,Messi)}")
-                #DELETING AN EXISTING USER FROM THE USERS TABLE
-    Jacob = session.query(User).filter_by(name = "Jacob Little").first()
-    print({delete_user(session, Jacob)})
+    #             #ADDING A NEW USER TO THE USERS TABLE
+    # Messi = User(name="Messi", email="messi@gmail.com", phone_number=254722806547)
+    # print(f"{add_user(session,Messi)}")
+    #             #DELETING AN EXISTING USER FROM THE USERS TABLE
+    # Jacob = session.query(User).filter_by(name = "Jacob Little").first()
+    # print({delete_user(session, Jacob)})
 
--- wrap.md
+++ wrap.md
@@ -69,3 +69,38 @@ class Book_checkout(Base):
 
     def __repr__(self):
         return repr({"id": self.id,"Book":self.book,  "genre": self.genre, "checkoutDate": self.checkoutDate, "returnDate": self.returnDate})
+
+this is my code in the models.py file . ths is the foundation of the book library CLI app I will be creating using python. 
+
+the database.py file will define all methods related to data manipulation for the database. The methods include
+ --methods for the book table--
+get_all_books() - gets information on all books
+add_book()
+update_book()
+delete_book()
+
+--methods for the user table--
+get _all_users() - gets information on all users
+add_user()
+delete_user()
+
+--methods for the Book_checkout table --
+get_all_checkouts()
+checkout_book()
+return_book()
+
+otherMethods
+reccomend_books() recomends books to user based on the genre of past book checkouts in the BookCheckout table
+
+the bookLibrary.py file is the application itself. all methods in the database .py will be called here as app commands . the app commands defined will be used as commands when interacting with the application itself.
+such commands include
+show _library() - calls the methods get_all_books() ,  get _all_users()  ,  get_all_checkouts() and displays the data in their respective tables.
+
+add_book()
+update_book()
+delete_book()
+add_user()
+delete_user()
+checkout_book()
+return_book()
+reccomend_books() - this methods will be app commands that will call the methods with the exact same name.
